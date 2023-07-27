53cfc38f4ef89e6d8f645ce3d973aa4496efc398
abimichel <abigail.michel@gov.bc.ca>
2023-07-26 20:11:29 -0700

    modified:   oracle_db_extract.py

--- oracle_db_extract.py
+++ oracle_db_extract.py
@@ -1,18 +1,23 @@
 import oracledb
 import pandas
 import os
+import boto3
+from dotenv import load_dotenv
 
 # oracledb.init_oracle_client(lib_dir="/opt/oracle/instantclient_21_10")
+query_file = "rar_query.sql"
 
 def load_configuration():
-
+    load_dotenv()
     username = os.getenv('DB_USERNAME')
     password = os.getenv('DB_PASSWORD')
     host = os.getenv('DB_HOST')
     port = os.getenv('DB_PORT')
     database = os.getenv('DATABASE')
+    objid = os.getenv('OBJ_STOR_ID')
+    objkey = os.getenv('OBJ_STOR_KEY')
 
-    return username, password, host, port, database
+    return username, password, host, port, database, objid, objkey
 
 def execute_query(connection, query_file):
     with open(query_file, 'r') as sql_file:
@@ -25,26 +30,40 @@ def execute_query(connection, query_file):
         df = pandas.DataFrame(results, columns=columns)
         return df
 
-# def export_to_excel(df, file_path):
-    try:
-        df.to_excel(file_path, index=False)
-        print(f"Results exported to {file_path}")
-    except Exception as e:
-        print(f"Error exporting results to {file_path}: {str(e)}")
+def export_to_excel(df, objid, objkey):
 
-# query_file = "rar_query.sql"
-# This is a file name for within the Docker container. The long term file name should be: f"output_{date.today().strftime('%Y-%m-%d')}_{query_file.replace('.sql', '')}.xlsx"
-# output_file_name = "rar_output.xlsx"
-# This is a path within the Docker conatiner. The long term destination should be: //objectstore.nrs.bcgov/datafoundations_prod/rar_query_output
-# output_file_path = f"/extracts/{output_file_name}"
+    endpoint_url= 'https://nrs.objectstore.gov.bc.ca' 
+    
+    bucketname = 'bvtnvt'
+
+    s3_client = boto3.client('s3', endpoint_url=endpoint_url, aws_access_key_id=objid, aws_secret_access_key=objkey)
+
+    s3_key = 'rar-query-output/rar_output_from_docker.xlsx'
+
+    excel_file_path = 'extracts/rar_output.xlsx'
+
+    try:
+        
+        s3_client.upload_file(Filename=df, Bucket=bucketname,Key=excel_file_path) 
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    print(f'Successfully uploaded {excel_file_path} to S3 bucket {bucketname} with key {s3_key}.')
+    
+    print(f'Here are all the objects currently stored in {bucketname}:')
+
+    session = boto3.Session(aws_access_key_id=objid, aws_secret_access_key=objkey)
+
+    s3_resource = session.resource('s3', endpoint_url=endpoint_url)
+
+    my_bucket = s3_resource.Bucket(bucketname)
+
+    for my_bucket_object in my_bucket.objects.all():
+        print(my_bucket_object.key)
 
 try:
-    username, password, host, port, database = load_configuration()
-
-    #ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    #ssl_context.options |= ssl.OP_SINGLE_DH_USE
-    #ssl_context.load_cert_chain(certfile=ssl_certfile)
-
+    username, password, host, port, database, objid, objkey = load_configuration()
 
     dsn = oracledb.makedsn(host=host, port=port, service_name=database)
 
@@ -56,8 +75,8 @@ try:
         df = execute_query(con, query_file)
         print("Query executed successfully")
 
-        # export_to_excel(df, output_file_path)
-        # print("Excel file exported successfully")
+        export_to_excel(df, objid, objkey)
+        
 
 except oracledb.DatabaseError as e:
     print("Database error:", str(e))
